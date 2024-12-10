422254f05005f9582c7719f868a3e79d2d250e7e
alfonso1410 <105889509+alfonso1410@users.noreply.github.com>
2024-12-09 23:36:12 -0600

    Initial commit

---
+++ Assets/CambiodeEscenas/CambioEscena.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambioEscena : MonoBehaviour
+{
+     private void Update(){
+        int escena_actual = SceneManager.GetActiveScene().buildIndex; ///0,1,2
+        if(Input.GetKeyDown(KeyCode.P)){
+            if(escena_actual == 0){
+                cambioDeEscena(1);
+            }
+            else if(escena_actual == 1){
+                cambioDeEscena(2);
+            }
+            else{
+                cambioDeEscena(0);
+            }
+        }
+    }
+
+
+    public void cambioDeEscena(int index_escena_ir){
+        SceneManager.LoadScene(index_escena_ir);
+
+    }
+}
---
+++ Assets/Scripts/cambiar_color_inspector.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cambiar_color_inspector : MonoBehaviour
+{
+    Renderer rend;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rend.material.color = Color.blue;
+    }
+}
